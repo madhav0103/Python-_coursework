327bd73c57b3f4a0e6823cd3858daf4dabecd16f
madhav0103 <150217930+madhav0103@users.noreply.github.com>
2025-07-02 04:46:02 +0000

    key

--- Practice/keyword.py
+++ Practice/keyword.py
@@ -4,4 +4,4 @@ print(keyword.kwlist)
 ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue',
 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-'''
+''' 
