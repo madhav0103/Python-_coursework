4003b951d5e134b14ee904df7d4a46c774f4ae02
madhav0103 <150217930+madhav0103@users.noreply.github.com>
2025-07-02 07:33:39 +0000

    operators.py

--- Classwork/4.operators.py
+++ Classwork/4.operators.py
@@ -19,3 +19,43 @@ print("Greater than(>):",a>b) #Greater than(>): True
 print("Less than(<):",a<b)#Less than(<): False
 print("Greater than or equal to(>=):",a>=b)#Greater than or equal to(>=): True
 print("Less than or equal to(<=):",a<=b)#Less than or equal to(<=): False
+
+
+#3.Assignment Operators
+a=20
+b=10
+print("Assign(=):",a) #Assign(=): 20
+a+=b 
+print("Add & Assign(+=):",a) #Add & Assign(+=): 30
+a-=b
+print("Subtract & Assign(-=):",a) #Subtract & Assign(-=): 20
+a*=b
+print("Multiply & Assign(=):",a) #Multiply & Assign(=): 200
+a/=b
+print("Division & Assign(/=):",a) #Division & Assign(/=): 20.0
+a//=b
+print("Floor Divison & Assign(//=):",a) #Floor Divison & Assign(//=): 2.0
+a%=b
+print("Modulus & Assign(%=):",a) #Modulus & Assign(%=): 2.0
+a**=b
+print("Exponentiate & Assign(*=):",a) #Exponentiate & Assign(*=): 1024.0
+
+
+#4.Logical Operators (Using Logic Gates)
+print("AND operator:",a>b and b>a) #AND operator: False
+print("OR operator:",a<b or b<a) #OR operator: True
+print("NOT operator:",not a>b) #NOT operator: False
+
+
+#5.Membership Operators
+a = [1,2,3]
+print("In operator:",1 in a) #In operator: True
+print("Not In operator:",5 not in a) #Not In operator: True
+
+
+#6.Identity Operators
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+print("Is operator:",a is b) #In operator: True
+print("Is Not operator:",a is not c) #Not In operator: True
