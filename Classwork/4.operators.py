23ced0e96183c3d002f70ead5099126f45ec3ab4
madhav0103 <150217930+madhav0103@users.noreply.github.com>
2025-07-02 05:48:02 +0000

    operators.py

---
+++ Classwork/4.operators.py
@@ -0,0 +1,10 @@
+#Arthmatic operators
+a=10
+b=20
+print("Addition(+):",a+b)
+print("Substraction(-):",a-b)
+print("Multiplication(*):",a*b)
+print("Division(/):",a/b)
+print("floor division(//):",a//b)
+print("Modulus(%):",a%b)
+print("Exponentiation(**):",a**b)
