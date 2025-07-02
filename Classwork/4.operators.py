4f877c954cce824f1597f145a8733425524b75f7
madhav0103 <150217930+madhav0103@users.noreply.github.com>
2025-07-02 06:14:34 +0000

    operators.py

--- Classwork/4.operators.py
+++ Classwork/4.operators.py
@@ -8,3 +8,14 @@ print("Division(/):",a/b)
 print("floor division(//):",a//b)
 print("Modulus(%):",a%b)
 print("Exponentiation(**):",a**b)
+
+
+#Comparision operators
+a=20
+b=10
+print("Equal to(==):",a==b) #Equal to(==): False
+print(" not Equal to(!=):",a!=b) #not Equal to(!=): True
+print("Greater than(>):",a>b) #Greater than(>): True
+print("Less than(<):",a<b)#Less than(<): False
+print("Greater than or equal to(>=):",a>=b)#Greater than or equal to(>=): True
+print("Less than or equal to(<=):",a<=b)#Less than or equal to(<=): False
